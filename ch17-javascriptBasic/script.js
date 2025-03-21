83d3f9b4dde9f6bc0e881d4a262ea592081436b6
hiddongs <yhd1227@naver.com>
2025-03-21 09:15:40 +0900

    hi_ddongs 브랜치에 변경 사항 추가

---
+++ ch17-javascriptBasic/script.js
@@ -0,0 +1,11 @@
+document.write("script.js 파일에서 호출");
+
+document.wirte('Hello World<br>');
+document.write("Hello JAVASCRIPT<br>");
+
+//일반데이터로 큰 따옴표
+document.write('오늘은 "수요일"<br>');
+document.write("내일은 \"목요일\"<br>");
+
+//일반 데이터로 작은 따옴표
+document.write('여기는\'서울\'<br>');
